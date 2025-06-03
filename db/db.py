1b47eb4eb8c9c63b6604bb8a10f1ff0c801d35f6
Albert Ariel Putra <happypacman36@gmail.com>
2025-06-03 20:32:41 +0800

    Fix uninitialized fields and support concurrency

--- db/db.py
+++ db/db.py
@@ -8,6 +8,20 @@ cred = credentials.Certificate('credential.json')
 app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+transaction = db.transaction()
+
+@firestore.transactional
+def add_order_to_customer(transaction, email, ticket_ref):
+    ref = db.collection("customers")
+    query = list(ref.where(filter=FieldFilter("email", "==", email)).get(transaction=transaction))
+    if not query:
+        newRef = db.collection("customers").document()
+        transaction.set(newRef, {
+            "email": email, 
+            "ticketIds": [ticket_ref.id],
+        })
+    else:
+        transaction.update(query[0].reference, {"ticketIds": firestore.ArrayUnion([ticket_ref.id])})
 
 def add_orders(orders):
     # Assumption: the cat_dict keys are catA, catB, catC following the naming in firestore
@@ -21,17 +35,7 @@ def add_orders(orders):
         if "catC" not in ticket:
             ticket["catC"] = 0
         ticket["code"] = ticket_id
+        ticket["checkedIn"] = False
+        ticket["seatConfirmed"] = False
         _, ref = db.collection("tickets").add(ticket)
-        cache = db.collection("customers").where(filter=FieldFilter("email", "==", email)).stream()
-        cache_id = None
-        cache_dict = None
-        for doc in cache:
-            cache_id = doc.id
-            cache_dict = doc.to_dict()
-        if not cache_id:
-            _, _ = db.collection("customers").add({
-                "email": email,
-                "ticketIds": [ref.id]
-            })
-        else:
-            db.collection("customers").document(cache_id).update({"ticketIds": cache_dict["ticketIds"] + [ref.id]})
+        add_order_to_customer(transaction, email, ref)
