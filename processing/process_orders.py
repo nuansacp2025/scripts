7a8cf8aab2f29a4a34a585d114d7d5661b37dbc4
Andrew Janong <andrewjanong@gmail.com>
2025-06-13 22:05:36 +0800

    Fix: fix qty parsing

--- processing/process_orders.py
+++ processing/process_orders.py
@@ -29,11 +29,9 @@ def read_orders(csv_content):
         email = row["Email"]
 
         order_description = row["Line Description"]
-        cat, qty_pax = parse_order_description(order_description)
+        cat, qty = parse_order_description(order_description)
         cat = "cat" + cat
         qty = qty * int(float(row["Quantity"]))
-
-        qty = qty_pax * int(row["Quantity"])
 
         ticket_id = generate_ticket_id(order_id, date_time)
         key = (ticket_id, email)
